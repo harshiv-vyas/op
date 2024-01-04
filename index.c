ec2e7937c3d4b1e98ec0681200cd674b6df23683
harshiv-vyas <hrvyas.2006@gmail.com>
2024-01-04 11:45:46 +0530

    main

---
+++ index.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i=1 ; i<=8 ; i++)
+    {
+        printf("*   ");
+    }
+    printf("\n");
+    for(int i=1 ; i<=8 ; i++)
+    {
+        printf("*   ");
+    }
+    printf("\n");
+    for(int i=1 ; i<=8 ; i++)
+    {
+        printf("*   ");
+    }
+    
+    
+
+    return 0;
+}
+
